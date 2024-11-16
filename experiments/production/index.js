ca85f8750adfd5ba83fdff4de6e86084cce7bb00
sarangjeong <sa3649@gmail.com>
2024-11-15 18:33:59 -0800

    Add proliferate setup

--- experiments/production/index.js
+++ experiments/production/index.js
@@ -2,16 +2,16 @@ let timeline = [
     consentFormSlide,
     overallInstructionSlide,
     ...createSession(1),
-    ...createSession(2),
-    ...createSession(3)
+    // ...createSession(2),
+    // ...createSession(3)
 ]; // Replaced experimentInstruction with overallInstruction
 
 jsPsych.init({
     timeline: timeline,
     show_progress_bar: true,
     auto_update_progress_bar: true,
-    message_progress_bar: "진행 상황"
-    // on_finish: function () {
-    //     jsPsych.data.displayData('csv');
-    // }
+    message_progress_bar: "진행 상황",
+    on_finish: function () {
+        proliferate.submit({ "trials": "test" });
+    }
 });
