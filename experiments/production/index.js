8b0b00166b119cc028f4f7544619b4049ff3cb6d
sarangjeong <sa3649@gmail.com>
2024-11-15 20:54:18 -0800

    Add participant_id

--- experiments/production/index.js
+++ experiments/production/index.js
@@ -1,11 +1,20 @@
 let timeline = [
     consentFormSlide,
     overallInstructionSlide,
-    // ...createSession(1, false),
+    ...createSession(1, false),
     thanksSlide
     // ...createSession(2),
     // ...createSession(3)
 ]; // Replaced experimentInstruction with overallInstruction
+
+// Function to get the query parameter by name
+function getQueryParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+// Retrieve the participant_id from the URL
+const participant_id = getQueryParameter('participant_id');
 
 jsPsych.init({
     timeline: timeline,
@@ -13,6 +22,9 @@ jsPsych.init({
     auto_update_progress_bar: true,
     message_progress_bar: "진행 상황",
     on_finish: function () {
-        proliferate.submit({ "trials": "test" });
+        proliferate.submit({ 
+            "participant_id": participant_id,
+            "experiment_data": jsPsych.data.get().json() 
+        });
     }
 });
