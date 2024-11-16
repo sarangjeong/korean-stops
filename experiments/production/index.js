ebbb29586fd159967b2cf1052ba9b6b30857cca2
sarangjeong <sa3649@gmail.com>
2024-11-15 21:11:09 -0800

    Production experiment ready

--- experiments/production/index.js
+++ experiments/production/index.js
@@ -1,10 +1,10 @@
 let timeline = [
     consentFormSlide,
     overallInstructionSlide,
-    ...createSession(1, false),
+    ...createSession(1),
+    ...createSession(2),
+    ...createSession(3, false),
     thanksSlide
-    // ...createSession(2),
-    // ...createSession(3)
 ]; // Replaced experimentInstruction with overallInstruction
 
 // Function to get the query parameter by name
@@ -22,8 +22,7 @@ jsPsych.init({
     auto_update_progress_bar: true,
     message_progress_bar: "진행 상황",
     on_finish: function () {
-        proliferate.submit({ 
-            "participant_id": participant_id,
+        proliferate.submit({
             "experiment_data": jsPsych.data.get().json() 
         });
     }
